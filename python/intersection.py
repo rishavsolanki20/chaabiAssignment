a9f851a97511e69f31d0674c9d5f8f7fce7a23aa
Rishav Raj <rr679122@gmail.com>
2023-06-09 00:53:31 +0530

    5th question added

--- python/intersection.py
+++ python/intersection.py
@@ -5,7 +5,7 @@ def compare_lists(a, b):
     symmetricDifference = list((aSet | bSet) - (aSet & bSet))
     return intersection, symmetricDifference
 
-mainstream = ["One Punch Man", "Attack On Titan", "One Piece", "Sword Art Online", "Bleach", "Dragon Ball Z", "One Piece"]
+mainStream = ["One Punch Man", "Attack On Titan", "One Piece", "Sword Art Online", "Bleach", "Dragon Ball Z", "One Piece"]
 must_watch = ["Full Metal Alchemist", "Code Geass", "Death Note", "Stein's Gate", "The Devil is a Part Timer!", "One Piece", "Attack On Titan"]
 
-print(compare_lists(mainstream, must_watch))
+print(compare_lists(mainStream, must_watch))
